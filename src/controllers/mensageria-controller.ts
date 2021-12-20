2be06df6bbbd13316839ca27935f41efe77ba53b
claudinei.braine <claudinei.braine@bioma.ind.br>
2021-12-20 16:27:03 -0300

    [Ajustes] AJuste host para testes no POSTMAN e SWAGGER

--- src/controllers/mensageria-controller.ts
+++ src/controllers/mensageria-controller.ts
@@ -56,8 +56,32 @@ const sendMailSchema = Joi.object().keys({
 // Email sending method
 const sendMail = async (req: any, res: any) => {
 
+    // Home test block in development
+    // Should be erased when going to production
+    let host_test = false
+
+    var array = req.rawHeaders;
+    var elemento = 'Postman-Token';
+    var idx = array.indexOf(elemento);
+
+    if (idx >= 0) {
+        // If you find it, it will be a positive value.
+        host_test = true
+    }
+ 
+    var elemento = 'http://localhost:5000/api/doc/';
+    var idx = array.indexOf(elemento);
+
+    if (idx >= 0) {
+        // If you find it, it will be a positive value.
+        host_test = true
+    }
+    //End block for testing locally in development
+
+
+
     const ip = require("ip")
-    
+
     // Captura host origem 
     const host = req.headers.referer
 
@@ -101,14 +125,14 @@ const sendMail = async (req: any, res: any) => {
             // each find a record
             if (user) {
                 // Checks if the host orihem is the same as the one allowed for the record
-                if ( (user.host == host) || (host == 'http://localhost:5000/api/doc/')) {
-                    
+                if ((user.host == host) || (host == 'http://localhost:5000/api/doc/') || (host_test == true)) {
+
                     // If it's a comma-separated list of emails, it turns it into an array
                     const list_emails = email.split(',')
 
                     // Initialized shipment status array
                     let status_send: { status: boolean; data: SentMessageInfo; }[] = []
-                    
+
                     // Scroll through the email list to send an individual
                     for await (let aux of list_emails) {
 
